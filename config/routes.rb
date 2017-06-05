e56d27edfcf717be8b7a7c4ae90b68911ee291d4
felipe quintana <felipe.q1989@gmail.com>
2017-06-05 16:07:46 +0200

    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/game', to: 'plays#game'
+
+  get '/score', to: 'plays#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'plays#game'
 end
